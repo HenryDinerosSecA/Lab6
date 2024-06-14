f192b9a2e12391307add2b0485eba3d2df3792cb
HenryDinerosSecA <n01661670@humber.ca>
2024-06-14 14:57:34 -0400

    initial build

---
+++ scripts.js
@@ -0,0 +1,28 @@
+/*Student Number - N01661670*/
+let myNumber = 10;
+let myString = "Hello, JavaScript!";
+
+    console.log(myNumber);
+    console.log(myString);
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(3 * i);
+}
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+let result = addNumbers(6, 9);
+console.log(result);
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+
+let product = multiplyNumbers(4, 20);
+console.log(product);
